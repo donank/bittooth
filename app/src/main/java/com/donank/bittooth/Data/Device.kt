e46fcc604bb36b82136ae452c7d7ec5534f26b40
donank <anandankit@protonmail.com>
2018-07-11 05:36:27 +0530

    Device Model, Recyclerviews for paired and available devices

---
+++ app/src/main/java/com/donank/bittooth/Data/Device.kt
@@ -0,0 +1,7 @@
+package com.donank.bittooth.Data
+
+data class Device(
+        val name : String,
+        val type : Int,
+        val mac : String
+)
--- app/src/main/java/com/donank/bittooth/Fragments/BluetoothDashboard.kt
+++ app/src/main/java/com/donank/bittooth/Fragments/BluetoothDashboard.kt
@@ -1,30 +1,78 @@
 package com.donank.bittooth.Fragments
 
 import android.bluetooth.BluetoothAdapter
+import android.bluetooth.BluetoothDevice
+import android.content.BroadcastReceiver
+import android.content.Context
 import android.content.Intent
+import android.content.IntentFilter
+import android.databinding.ObservableArrayList
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.donank.bittooth.Activity.MainActivity
+import com.donank.bittooth.BR
+import com.donank.bittooth.Data.Device
 import com.donank.bittooth.R
 import com.donank.bittooth.Utility.showFragment
 import com.donank.bittooth.Utility.showInSnack
+import com.donank.bittooth.databinding.ItemDeviceBinding
+import com.github.nitrico.lastadapter.LastAdapter
 import kotlinx.android.synthetic.main.fragment_bluetooth_dashboard.*
 
 class BluetoothDashboard : Fragment() {
 
-    val bluetoohAdapter = BluetoothAdapter.getDefaultAdapter()
+    private val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
 
-    val REQUEST_ENABLE_BT = 1
-    val REQUEST_DISABLE_BT = 2
+    private val REQUEST_ENABLE_BT = 1
+
+    private val pairedDevices = ObservableArrayList<Device>()
+
+    private val availableDevices = ObservableArrayList<Device>()
+
+    private val pairedLastAdapter : LastAdapter by lazy {
+        initPairedLastAdapter()
+    }
+
+    private fun initPairedLastAdapter(): LastAdapter{
+        return LastAdapter(pairedDevices, BR.item)
+                .map<Device, ItemDeviceBinding>(R.layout.item_device)
+                .into(recycler_paired_devices)
+    }
+    private val availableDevicesLastAdapter : LastAdapter by lazy {
+        initAvailableDevicesLastAdapter()
+    }
+
+    private fun initAvailableDevicesLastAdapter(): LastAdapter{
+        return LastAdapter(availableDevices, BR.item)
+                .map<Device, ItemDeviceBinding>(R.layout.item_device)
+                .into(recycler_available_devices)
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        if (bluetoohAdapter == null) {
+        if (bluetoothAdapter == null) {
             showInSnack(this.view!!,"Bluetooth support not available! Exiting!!")
             activity!!.finish()
+        }
+
+        activity!!.registerReceiver(mReceiver,IntentFilter(BluetoothDevice.ACTION_FOUND))
+    }
+
+    private val mReceiver = object : BroadcastReceiver() {
+        override fun onReceive(context: Context, intent: Intent) {
+            val action = intent.action
+            if (BluetoothDevice.ACTION_FOUND == action) {
+                val device = intent.getParcelableExtra<BluetoothDevice>(BluetoothDevice.EXTRA_DEVICE)
+                availableDevices.add(Device(
+                        name = device.name,
+                        type = device.type,
+                        mac = device.address
+                ))
+                availableDevicesLastAdapter.notifyDataSetChanged()
+            }
         }
     }
 
@@ -33,20 +81,48 @@ class BluetoothDashboard : Fragment() {
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
         super.onActivityCreated(savedInstanceState)
-        if(!bluetoohAdapter.isEnabled){
+
+        recycler_paired_devices.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false)
+        recycler_paired_devices.adapter = pairedLastAdapter
+        recycler_available_devices.layoutManager = LinearLayoutManager(activity,LinearLayoutManager.VERTICAL,false)
+        recycler_available_devices.adapter = availableDevicesLastAdapter
+
+        if(!bluetoothAdapter.isEnabled){
             switch_togleBT.isChecked = false
         }
+
+        val storedPairedDevices = bluetoothAdapter.bondedDevices
+
+        if(storedPairedDevices.size > 0){
+            storedPairedDevices.forEach {
+                pairedDevices.add(Device(
+                        name = it.name,
+                        type = it.type,
+                        mac = it.address
+                ))
+                pairedLastAdapter.notifyDataSetChanged()
+            }
+        }else {
+            pairedDevices.add(Device(
+                    name = "No Paired Devices",
+                    type = 0,
+                    mac = ""
+            ))
+            pairedLastAdapter.notifyDataSetChanged()
+        }
+
         switch_togleBT.setOnCheckedChangeListener { buttonView, isChecked ->
             if(isChecked){
-                if(!bluetoohAdapter.isEnabled){
+                if(bluetoothAdapter.isEnabled){
                     startActivityForResult(
                             Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE),
                             REQUEST_ENABLE_BT
                     )
                 }
             }else{
-                if(bluetoohAdapter.isEnabled){
-                    bluetoohAdapter.disable()
+                if(!bluetoothAdapter.isEnabled){
+                    bluetoothAdapter.disable()
+                    showInSnack(this.view!!, "Bluetooth Disabled!")
                 }
             }
         }
@@ -69,4 +145,10 @@ class BluetoothDashboard : Fragment() {
                 fragmentManager = activity!!.supportFragmentManager,
                 addToBackStack = addToBackStack)
     }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        activity!!.unregisterReceiver(mReceiver)
+    }
 }
+
